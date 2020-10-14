d5db19b469d82cc9528b63037864eb1909eb930d
Timmy Jose <zoltan.jose@gmail.com>
2020-10-14 19:38:25 +0530

    chapter 3 - wrapup

---
+++ chapter3/function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <functional>
+
+int foo(int para) {
+    return para;
+}
+
+int main() {
+    std::function<int(int)> func = foo;
+
+    int important = 10;
+    std::function<int(int)> func2 = [&](int value) -> int {
+        return 1 + value + important;
+    };
+
+    std::cout << func(10) << std::endl;
+    std::cout << func2(10) << std::endl;
+
+    return 0;
+}
